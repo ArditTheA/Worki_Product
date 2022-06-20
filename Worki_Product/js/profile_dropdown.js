8731e9dea40e775a4d506a2a14c1819eddbabc71
ArditTheA <107107919+ArditTheA@users.noreply.github.com>
2022-06-20 19:37:27 +0200

    Add files via upload

--- Worki_Product/js/profile_dropdown.js
+++ Worki_Product/js/profile_dropdown.js
@@ -1,5 +1,5 @@
 var dropdown = document.querySelector(".dropdown");
-var profile = document.querySelector("#profile_image-button");
+var profile = document.querySelector("#profile-image-button");
 
 profile.addEventListener("click", function() {
    dropdown.classList.toggle("showDropdown");
